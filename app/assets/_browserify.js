0123d2250f987dd7e51106f2e5daf1b13b24f001
Vincent De Oliveira <vincent@iamvdo.me>
2014-07-02 19:34:13 +0200

    Update 0.4.1

--- app/assets/_browserify.js
+++ app/assets/_browserify.js
@@ -1,25 +1,29 @@
 var pleeease = require('pleeease');
+var prismjs  = require('prismjs');
 
 var input = document.querySelector('#input');
 var output = document.querySelector('#output');
+
+if(location.search !== '') {
+  var q = location.search.substring(1);
+  input.value = decodeURI(q);
+}
 
 var options = {
 	"optimizers": {
 		"minifier": false
 	}
 };
+function htmlEntities(str) {
+  return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
 var doPleeease = function () {
   var source = input.value;
   var compiled = source;
   try {
     compiled = pleeease.process(source, options);
   } catch (err) {}
-  output.value = compiled;
+  output.innerHTML = prismjs.highlight(htmlEntities(compiled), prismjs.languages.css);
 };
 doPleeease();
 input.addEventListener('keyup', doPleeease);
-
-if(location.search !== '') {
-  var q = location.search.substring(1);
-  input.value = decodeURI(q);
-}
